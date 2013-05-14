af7886567d84c513cbb4406fb4ccd800f9d39bc7
Evan <evan@Evans-MacBook-Pro.local>
2013-05-13 23:48:10 -0400

    week2.0

---
+++ Week1/js/main.js
@@ -0,0 +1,17 @@
+//Evan Combs
+//ASDI 1305
+
+$('#home').on('pageinit', function()
+{
+	//code for home page
+});
+
+$('#addItem').on('pageinit', function()
+{
+	//code for add/edit item page will go here
+});
+
+$('#viewItems').on('pageinit', function()
+{
+	//code for view items page
+});
